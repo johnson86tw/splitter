7176b9ea2767f2aac2e50cfbd4b9f5eaa1266bcb
Johnson Chen <chnejohnson@gmail.com>
2021-07-17 17:52:29 +0800

    dev: hardhat add typescript and typechain

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,4 +1,7 @@
-require("@nomiclabs/hardhat-waffle");
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-typechain";
+import "@nomiclabs/hardhat-ethers";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
--- contracts/test/Greeter.ts
+++ contracts/test/Greeter.ts
@@ -1,9 +1,13 @@
-const { expect } = require("chai");
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Greeter } from "../typechain/Greeter";
+
+let greeter: Greeter;
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
+    greeter = (await Greeter.deploy("Hello, world!")) as Greeter;
     await greeter.deployed();
 
     expect(await greeter.greet()).to.equal("Hello, world!");
