fa7d7da34e2a0c79882edfd3b85759a8e232bcaa
Johnson Chen <chnejohnson@gmail.com>
2021-07-23 16:59:52 +0800

    fix: refactor greeter

--- frontend/src/composables/greeter.ts
+++ frontend/src/composables/greeter.ts
@@ -27,64 +27,64 @@ function clearState() {
   errMsg.value = "";
 }
 
-export default function useGreeterContract() {
-  watch(hasSetupWallet, async hasSetupWallet => {
-    clearState();
+watch(hasSetupWallet, async hasSetupWallet => {
+  clearState();
 
-    if (hasSetupWallet && signer.value && chainId.value) {
-      if (supportedChainIds.includes(chainId.value)) {
-        console.log("createContract when setting up new wallet");
-        createContract(signer.value, chainId.value);
-        await getGreeting();
-      }
-    }
-  });
-
-  // reactive with useMetaMask
-  function createContract(signer: JsonRpcSigner, chainId: number) {
-    if (supportedChainIds.includes(chainId)) {
-      const contractAddress = greeterAddress[NETWORK(chainId)!.name];
-      const _contract = new ethers.Contract(contractAddress, contractData.abi, signer) as Greeter;
-      greeter.value = markRaw(_contract);
-    } else {
-      throw new Error("createContract: unsupported chainId");
-    }
-  }
-
-  function connectContractAt(address: string) {
-    let contract;
-    if (!isAddress(address)) throw new Error("address is invalid");
-    if (hasSetupWallet.value && signer.value && chainId.value) {
-      // @issue: throw Error can't be handled when address is not valid.
-      contract = new ethers.Contract(address, contractData.abi, signer.value) as Greeter;
-    }
-    return contract;
-  }
-
-  async function getGreeting() {
-    if (!greeter.value) return;
-    errMsg.value = "";
-    try {
-      greet.value = await greeter.value.greet();
-    } catch (e) {
-      errMsg.value = e.message;
-    }
-  }
-
-  async function setGreeting(greet: string) {
-    if (!greeter.value) return;
-    errMsg.value = "";
-    try {
-      const tx = await greeter.value.setGreeting(greet);
-      // @todo add tx pending state
-      await tx.wait();
+  if (hasSetupWallet && signer.value && chainId.value) {
+    if (supportedChainIds.includes(chainId.value)) {
+      console.log("createContract when setting up new wallet");
+      createContract(signer.value, chainId.value);
       await getGreeting();
-      await getBalance();
-    } catch (e) {
-      errMsg.value = e.message;
     }
   }
+});
 
+// reactive with useMetaMask
+function createContract(signer: JsonRpcSigner, chainId: number) {
+  if (supportedChainIds.includes(chainId)) {
+    const contractAddress = greeterAddress[NETWORK(chainId)!.name];
+    const _contract = new ethers.Contract(contractAddress, contractData.abi, signer) as Greeter;
+    greeter.value = markRaw(_contract);
+  } else {
+    throw new Error("createContract: unsupported chainId");
+  }
+}
+
+function connectContractAt(address: string) {
+  let contract;
+  if (!isAddress(address)) throw new Error("address is invalid");
+  if (hasSetupWallet.value && signer.value && chainId.value) {
+    // @issue: throw Error can't be handled when address is not valid.
+    contract = new ethers.Contract(address, contractData.abi, signer.value) as Greeter;
+  }
+  return contract;
+}
+
+async function getGreeting() {
+  if (!greeter.value) return;
+  errMsg.value = "";
+  try {
+    greet.value = await greeter.value.greet();
+  } catch (e) {
+    errMsg.value = e.message;
+  }
+}
+
+async function setGreeting(greet: string) {
+  if (!greeter.value) return;
+  errMsg.value = "";
+  try {
+    const tx = await greeter.value.setGreeting(greet);
+    // @todo add tx pending state
+    await tx.wait();
+    await getGreeting();
+    await getBalance();
+  } catch (e) {
+    errMsg.value = e.message;
+  }
+}
+
+export default function useGreeterContract() {
   return {
     errMsg,
     greet,
