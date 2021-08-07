3343636ff10e7e31432f67376ac78ae457f04930
Johnson Chen <chnejohnson@gmail.com>
2021-08-07 11:28:09 +0800

    feat: notify animation

--- frontend/src/components/Notification.vue
+++ frontend/src/components/Notification.vue
@@ -45,34 +45,39 @@ export default defineComponent({
 
 <template>
   <teleport to="html">
-    <div
-      v-if="state.isNotifying"
-      class="fixed right-0 bottom-0"
+    <transition
+      appear
+      name="fade-down"
     >
-      <!-- Toast Notification Success-->
-      <div class="flex items-center bg-green-500 border-l-4 border-green-700 py-2 px-3 shadow-md mb-2">
-        <div class="text-green-500 rounded-full bg-white mr-3">
-          <svg
-            width="1.8em"
-            height="1.8em"
-            viewBox="0 0 16 16"
-            class="bi bi-check"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"
-            />
-          </svg>
-        </div>
-        <div class="text-white max-w-xs ">
-          {{ state.message }}
-        </div>
-      </div>
 
-      <!-- Toast Notification Info -->
-      <!-- <div class="flex items-center bg-blue-400 border-l-4 border-blue-700 py-2 px-3 shadow-md mb-2">
+      <div
+        v-if="state.isNotifying"
+        class="fixed right-0 bottom-0"
+      >
+        <!-- Toast Notification Success-->
+        <div class="flex items-center bg-green-500 border-l-4 border-green-700 py-2 px-3 shadow-md mb-2">
+          <div class="text-green-500 rounded-full bg-white mr-3">
+            <svg
+              width="1.8em"
+              height="1.8em"
+              viewBox="0 0 16 16"
+              class="bi bi-check"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"
+              />
+            </svg>
+          </div>
+          <div class="text-white max-w-xs ">
+            {{ state.message }}
+          </div>
+        </div>
+
+        <!-- Toast Notification Info -->
+        <!-- <div class="flex items-center bg-blue-400 border-l-4 border-blue-700 py-2 px-3 shadow-md mb-2">
         <div class="text-blue-500 rounded-full bg-white mr-3">
           <svg
             width="1.8em"
@@ -95,8 +100,8 @@ export default defineComponent({
         </div>
       </div> -->
 
-      <!-- Toast Notification Warning -->
-      <!-- <div class="flex items-center bg-orange-400 border-l-4 border-orange-700 py-2 px-3 shadow-md mb-2">
+        <!-- Toast Notification Warning -->
+        <!-- <div class="flex items-center bg-orange-400 border-l-4 border-orange-700 py-2 px-3 shadow-md mb-2">
         <div class="text-orange-500 rounded-full bg-white mr-3">
           <svg
             width="1.8em"
@@ -114,8 +119,8 @@ export default defineComponent({
         </div>
       </div> -->
 
-      <!-- Toast Notification Danger -->
-      <!-- <div class="flex items-center bg-red-500 border-l-4 border-red-700 py-2 px-3 shadow-md mb-2">
+        <!-- Toast Notification Danger -->
+        <!-- <div class="flex items-center bg-red-500 border-l-4 border-red-700 py-2 px-3 shadow-md mb-2">
         <div class="text-red-500 rounded-full bg-white mr-3">
           <svg
             width="1.8em"
@@ -139,6 +144,20 @@ export default defineComponent({
           ini pesan ketika ada error
         </div>
       </div> -->
-    </div>
+      </div>
+    </transition>
+
   </teleport>
 </template>
+
+<style scoped>
+.fade-down-enter-active,
+.fade-down-leave-active {
+  transition: all 0.5s;
+}
+.fade-down-enter-from,
+.fade-down-leave-to {
+  opacity: 0;
+  transform: translateX(50px);
+}
+</style>
