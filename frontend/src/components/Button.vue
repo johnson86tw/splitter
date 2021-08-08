d8768e280e4c7ea7ff28c72e0425f9a76140d6d0
Johnson Chen <chnejohnson@gmail.com>
2021-08-08 13:36:30 +0800

    add: Button component updated

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -1,15 +1,79 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { useLoader } from "./Loader.vue";
+
+export default defineComponent({
+  props: {
+    handlerFn: {
+      type: Function,
+      required: true,
+    },
+    globalLoader: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const { isLoading: isGlobalLoading } = useLoader();
+    const isLoading = ref(false);
+    const errMsg = ref("");
+
+    const handler = async () => {
+      errMsg.value = "";
+      props.globalLoader
+        ? (isGlobalLoading.value = true)
+        : (isLoading.value = true);
+      try {
+        await props.handlerFn();
+      } catch (e) {
+        errMsg.value = e.message;
+      } finally {
+        props.globalLoader
+          ? (isGlobalLoading.value = false)
+          : (isLoading.value = false);
+      }
+    };
+    return {
+      isLoading,
+      errMsg,
+      handler,
+    };
+  },
+});
+</script>
+
+
 <template>
   <button
+    @click="handler"
     type="button"
-    class="btn"
-    disabled
+    :disabled="isLoading ? true : false"
+    class="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 
+    font-medium rounded-md text-gray-600 text-xl bg-blue-100 hover:bg-blue-200 focus:outline-none 
+    transition ease-in-out duration-150 disabled:cursor-default disabled:opacity-70 disabled:bg-blue-200"
   >
     <svg
-      class="animate-spin h-5 w-5 mr-3 ..."
+      v-if="isLoading"
+      class="animate-spin -ml-1 mr-3 h-5 w-5 text-blue-600"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
       viewBox="0 0 24 24"
     >
-      <!-- ... -->
+      <circle
+        class="opacity-20"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      ></circle>
+      <path
+        class="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
     </svg>
-    Processing
+    <slot />
   </button>
+  <p class="text-red-600">{{ errMsg }}</p>
 </template>
